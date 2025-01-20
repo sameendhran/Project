9bff6f9b5384a995a32ab79e7a37f0b8277fb09b
sri_sameendhran <146868947+sameendhran@users.noreply.github.com>
2025-01-20 22:09:51 +0530

    Put and Delete Mapping

---
+++ BackEnd/8.10 Put and Delete Mapping/spring-boot-rest4/src/main/java/com/telusko/springbootrest/controller/JobRestController.java
@@ -0,0 +1,49 @@
+package com.telusko.springbootrest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.telusko.springbootrest.service.JobService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class JobRestController {
+	
+	@Autowired
+	private JobService service;
+	
+	
+	@GetMapping("jobPosts")
+	public List<JobPost> getAllJobs() {
+		return service.getAllJobs();
+		
+	}
+	
+	@GetMapping("/jobPost/{postId}")
+	public JobPost getJob(@PathVariable int postId) {
+		return service.getJob(postId);
+	}
+
+	@PostMapping("jobPost")
+	public JobPost addJob(@RequestBody JobPost jobPost) {
+		service.addJob(jobPost);
+		return service.getJob(jobPost.getPostId());
+	}
+	
+	@PutMapping("jobPost")
+	public JobPost updateJob(@RequestBody JobPost jobPost) {
+		service.updateJob(jobPost);
+		return service.getJob(jobPost.getPostId());
+	}
+	
+	@DeleteMapping("jobPost/{postId}")
+	public String deleteJob(@PathVariable int postId)
+	{
+		service.deleteJob(postId);
+		return "Deleted";
+	}
+	
+	
+}
